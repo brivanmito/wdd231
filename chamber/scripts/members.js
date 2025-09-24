d149c6f21a9ec68f08f90bf61fdd0768ec9c00dd
Bryan Mino Lenovo <bryan_royce1997@hotmail.com>
2025-09-24 02:52:31 -0500

    Creating info por members

---
+++ chamber/scripts/members.js
@@ -0,0 +1,12 @@
+
+//Create a function using fecth and async await
+
+async function GetMembers() {
+    try {
+
+        
+        
+    } catch (error) {
+        console.error("Error fetching Data ", error );
+    }
+}
