743e1a2d83278fb7993f58564ff23a9d4bd80ccd
Bryan Mino Lenovo <bryan_royce1997@hotmail.com>
2025-10-04 04:49:50 -0500

    Updating the json file with a new item called tagline

--- chamber/scripts/randommembers.js
+++ chamber/scripts/randommembers.js
@@ -29,9 +29,76 @@ async function GetAPI() {
 }
 
 
-function GetMembersSpotligth() {
+async function GetMembersSpotligth() {
+
+    // Get the seed for rabndom companys
     
     const quantityMembers = GetRandomQuantity();
 
+    const dataMembers = await GetAPI();
+
+    const silverMembers = GetMembersByCategory(dataMembers, "silver");
+
+    const goldMembers = GetMembersByCategory(dataMembers, "gold");
+
+    const membersChosen = silverMembers.concat(goldMembers);
+
+    // Random Array
+
+    const randomArray = membersChosen.sort(() => Math.random() - 0.5);
+
+    
+
+    for (let i = 0; i < quantityMembers; i++) {
+
+
+        PopulateData(randomArray[i]);
+
+
+
+    }
+
+
+
     
 }
+
+function PopulateData(arrayInfo) {
+
+    // Create a DIV element 
+    const divElement = document.createElement("div");
+
+    const h3Element = document.createElement("h3");
+
+    const pdescription = document.createElement("p");
+
+    const divContainerImgText = document.createElement("div");
+
+    const photoLogo = document.createElement("img");
+
+    const pinfoBussiness = document.createElement("p");
+
+    h3Element.textContent = arrayInfo.name;
+
+    pdescription.textContent = arrayInfo.tagline;
+
+
+    console.log(arrayInfo.name);
+    console.log(arrayInfo.tagline);
+
+
+
+
+
+}
+
+function GetMembersByCategory(datamembers, category) {
+
+    return datamembers.members.filter(m => m.membershiplevel.toLowerCase() === category.toLowerCase());
+}
+
+function PopulateMembersSpotLigth() {
+
+}
+
+GetMembersSpotligth();
