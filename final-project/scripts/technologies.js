114535f7e8deb6ed766cecfff3f7e7c62887f3a0
Bryan Mino <bryanmt496@gmail.com>
2025-10-15 17:35:25 -0500

    uploading files

--- final-project/scripts/technologies.js
+++ final-project/scripts/technologies.js
@@ -1,12 +1,49 @@
 const container = document.querySelector("#technologies-container");
 
-const 
+const url = "https://raw.githubusercontent.com/brivanmito/wdd231/main/final-project/data/technologies.json";
 
 export function LoadTechnologies() {
 
+    FetchApi();
 
 }
 
 async function FetchApi() {
+    try {
+
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        DisplayTechnologies(data.technologies);
+        
+    } catch (error) {
+
+        console.error("Error fetching Data ", error );
+    }
+}
+
+function DisplayTechnologies(technologies) {
+
+
+    technologies.forEach(technology => {
+
+        const div = document.createElement("div");
+
+        const img = document.createElement("img");
+
+        const title = document.createElement("p");
+
+        img.setAttribute("src", technology.imageUrl);
+        img.setAttribute("alt", `${technology.title} Logo`);
+
+        title.textContent = technology.title;
+
+        div.append(img, title);
+
+        container.appendChild(div);
+    });
 
 }
+
+
