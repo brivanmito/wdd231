a2548085622760bc651b33d3e0db68a31ecc04d3
Bryan Mino <bryanmt496@gmail.com>
2025-10-15 22:57:22 -0500

    Hello

--- final-project/scripts/app.js
+++ final-project/scripts/app.js
@@ -1,7 +1,13 @@
 import { AddHideNavigation } from "./navigationmenu.js";
 
-import {  LoadTechnologies } from "./technologies.js";
+import { LoadTechnologies } from "./technologies.js";
+
+import { DisplayEducation } from "./education.js";
+
 
 AddHideNavigation();
 
 LoadTechnologies();
+
+DisplayEducation();
+
---
+++ final-project/scripts/education.js
@@ -0,0 +1,73 @@
+const container = document.querySelector("#section-education");
+
+const url = "https://raw.githubusercontent.com/brivanmito/wdd231/main/final-project/data/education.json";
+
+export function LoadEducation() {
+
+    FetchApi();
+
+}
+
+async function FetchApi() {
+    try {
+
+        const response = await fetch(url);
+
+        const data = await response.json();
+
+        DisplayEducation(data.education);
+        
+    } catch (error) {
+
+        console.error("Error fetching Data ", error );
+    }
+}
+
+function DisplayEducation(education) {
+
+
+    education.forEach(institucion => {
+
+        const divContainer = document.createElement("div");
+
+        const divImg = document.createElement("div");
+
+        const divText = document.createElement("div");
+
+        const img = document.createElement("img");
+
+        const universityName = document.createElement("h3");
+
+        const degreeTitle = document.createElement("p");
+
+        const period = document.createElement("p");
+
+        const languages = document.createElement("p");
+
+
+
+        img.setAttribute("src", institucion.urlimg);
+        img.setAttribute("alt", `${institucion.name} Logo`);
+        img.setAttribute("height", "100");
+        img.setAttribute("width", "100");
+        img.setAttribute("loading", "lazy");
+
+        degreeTitle.textContent = institucion.grade_title;
+
+        universityName.textContent = institucion.name;
+
+        period.textContent = `Period: `;
+
+        languages.textContent = `Languages: `;
+
+        divImg.append(img);
+
+        divText.append(universityName, degreeTitle, period, languages);
+
+        divContainer.append(divImg, divText);
+
+    });
+
+}
+
+
